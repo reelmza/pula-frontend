47d3f288e0fcd861fed15ea3bc1969b0c7786690
reelmza <jessemoses71@gmail.com>
2025-06-04 16:52:33 +0100

    Typography corrections

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -20,19 +20,20 @@ const Page = () => {
       <h1 className=" text-4xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-emerald-700 via-emerald-600 to-emerald-700 font-extrabold tracking-tight mb-2">
         Documentation
       </h1>
+
       {/* Description */}
       <p className="w-full lg:w-[35%] px-5 text-sm text-gray-600 text-center mb-10">
-        Find detailed guides about the working principles of the model.
+        Find detailed guides about the working principles of the model below.
       </p>
       <div className="flex flex-col w-full px-5 lg:w-4/10 lg:gap-4">
         <Section
           title="Specifications"
-          description="The model undestands e-mails written in english langueage only and the model predicts the class of an e-mail based on factors such ass grammatical accuracy, sentence form, use of punctuations, mispellings and length of e-mail."
+          description="The model understands e-mails written in english language only and the model predicts the class of an e-mail based on factors such as grammatical accuracy, sentence form, use of punctuations, mispellings and length of e-mail."
         />
 
         <Section
           title="Limitations and Working"
-          description="The project was built to be attain as much accuracy as possible, however, false possitives and false negatives will be encountered occasionally due to the structure of the email provided and other factors such as language."
+          description="The project was built to attain as much accuracy as possible, however, false possitives and false negatives will be encountered occasionally due to the structure of the email provided and other factors such as language."
         />
 
         <Section
@@ -43,15 +44,15 @@ const Page = () => {
         />
         <Section
           title="About The Model"
-          description="The model is trained with english data sets from kaggle, pyhthon modules such as nunmpy, pandas and sklearn were heavily utilised during the development of the model."
+          description="The model is trained with english data sets from kaggle, pyhhon modules such as nunmpy, pandas and sklearn were heavily utilised during the development of the model."
         />
 
         <div className="h-5"></div>
 
         <div className="text-center text-sm text-gray-600">
-          Built By Pula as partial requiredment for the award of Bachelors
-          Degree in Cyber Security at Bingham University, Karu, Nasarawa State,
-          Nigeria.{" "}
+          Built By ELIJAH IKO-OJO IBRAHIM as a partial requirement for the award
+          of Bachelors Degree in Cyber Security at Bingham University, Karu,
+          Nasarawa State, Nigeria.{" "}
         </div>
 
         <div className="h-20"></div>
